0742796317b40ab0705f3eb72da546b9c2450d14
Paul Callahan <paul@tcell.io>
2018-02-22 20:40:11 -0800

    weed out symbols incompatible with 2.2

--- src/ffi.rs
+++ src/ffi.rs
@@ -261,10 +261,16 @@ pub fn strdup<T: Into<Vec<u8>>>(pool: *mut apr_pool_t, data: T) -> *mut c_char {
 
 // APACHE HTTPD
 
-pub const MODULE_MAGIC_COOKIE: c_ulong = 0x41503234u64; /* "AP24" */
+//for 2.4, was:
+//pub const MODULE_MAGIC_COOKIE: c_ulong = 0x41503234u64; /* "AP24" */
+//pub const MODULE_MAGIC_NUMBER_MAJOR: c_int = 20120211;
+//pub const MODULE_MAGIC_NUMBER_MINOR: c_int = 36;
 
-pub const MODULE_MAGIC_NUMBER_MAJOR: c_int = 20120211;
-pub const MODULE_MAGIC_NUMBER_MINOR: c_int = 36;
+// 2.2
+pub const MODULE_MAGIC_COOKIE: c_ulong = 0x41503232u64; /* "AP22" */
+pub const MODULE_MAGIC_NUMBER_MAJOR: c_int = 20051115;
+pub const MODULE_MAGIC_NUMBER_MINOR: c_int = 43;
+
 
 pub const OK:        c_int = 0;
 pub const DECLINED:  c_int = -1;
